9d12d81a7cf7ff4f14add3e88d0f51d909ecc1d4
Ham <60859010+Hamish-Robinson@users.noreply.github.com>
2020-05-25 11:30:13 +1200

    25/05/2020

--- Assets/Scripts/YouWinButton.cs
+++ Assets/Scripts/YouWinButton.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
 
 public class YouWinButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void changemenuscene()
     {
-        
-    }
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
     }
 }
